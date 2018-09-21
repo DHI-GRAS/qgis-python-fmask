410bc8d343b1dcf8e71d2d82da20a250ab65e153
Jonas Sølvsteen <j08lue@gmail.com>
2018-09-21 11:21:40 +0200

    verbose old place

--- qgis_fmask_sentinel2Stacked.py
+++ qgis_fmask_sentinel2Stacked.py
@@ -5,13 +5,13 @@
 ##ParameterFile|granuledir|Path to .SAFE or tile/granule directory|True|False
 ##ParameterFile|anglesfile|Input angles file containing satellite and sun azimuth and zenith|False|True|
 ##OutputFile|output|Output cloud mask|tif
+##*ParameterBoolean|verbose|verbose output|True
 ##ParameterNumber|mincloudsize|Mininum cloud size (in pixels) to retain, before any buffering|0|None|0
 ##ParameterNumber|cloudbufferdistance|Distance (in metres) to buffer final cloud objects|0|None|150
 ##ParameterNumber|shadowbufferdistance|Distance (in metres) to buffer final cloud shadow objects|0|None|300
 ##ParameterNumber|cloudprobthreshold|Cloud probability threshold (percentage) (Eqn 17)|0|100|20
 ##*ParameterNumber|nirsnowthreshold|Threshold for NIR reflectance for snow detection (Eqn 20). Increase this to reduce snow commission errors|0|1|0.11
 ##*ParameterNumber|greensnowthreshold|Threshold for Green reflectance for snow detection (Eqn 20). Increase this to reduce snow commission errors|0|1|0.1
-##*ParameterBoolean|verbose|verbose output|True
 
 from argparse import Namespace
 import sys
