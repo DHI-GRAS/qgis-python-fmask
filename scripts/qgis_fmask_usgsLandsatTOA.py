9078bf2cbf2f6ac390c7c03d8312e228d1a7b5d6
Nicolai <nith@dhigroup.com>
2020-06-11 16:01:36 +0200

    TOA to qgis scripts no bug

--- scripts/qgis_fmask_usgsLandsatTOA.py
+++ scripts/qgis_fmask_usgsLandsatTOA.py
@@ -16,10 +16,6 @@ import tempfile
 import numpy as np
 from processing.tools import dataobjects
 
-here = os.path.dirname(scriptDescriptionFile)
-if here not in sys.path:
-    sys.path.append(here)
-
 from qgis_fmask.stacks.landsat_stack import create_landsat_stack
 from qgis_fmask.interfaces.fmask_usgsLandsatTOA import mainRoutine
 from qgis_fmask.interfaces.fmask_usgsLandsatMakeAnglesImage import (
@@ -28,43 +24,62 @@ from qgis_fmask.interfaces.fmask_usgsLandsatMakeAnglesImage import (
 from qgis_fmask.interfaces.fmask_usgsLandsatSaturationMask import (
     mainRoutine as mainRoutine_saturation,
 )
-from qgis_fmask.interfaces.redirect_print import redirect_print
+from qgis_fmask.interfaces.redirect_stdout import redirect_stdout_to_feedback
 from qgis_fmask.interfaces.landsatmeta import find_mtl_in_product_dir
+from qgis.processing import alg
 
-landsatkey = ["4&5", "7", "8"][landsatkeynr]
+@alg(
+    name="landsattopofatmosphere",
+    label=alg.tr("Landsat Top Of Atmosphere"),
+    group="notupdated",
+    group_label=alg.tr("Not Updated"),
+)
+@alg.input(
+    type=alg.FILE,
+    name="infile",
+    label="Infile",
+    behavior=0,
+    optional=False,
+    fileFilter="tif",
+)
+@alg.input(type=alg.FILE_DEST, name="outfile", label="Outfile")
+@redirect_stdout_to_feedback
+def landsattopofatmosphere(instance, parameters, context, feedback, inputs):
+    """ landsattopofatmosphere """
+    landsatkey = ["4&5", "7", "8"][landsatkeynr]
 
-tempdir = tempfile.mkdtemp()
-try:
-    mtl = find_mtl_in_product_dir(productdir)
+    tempdir = tempfile.mkdtemp()
+    try:
+        mtl = find_mtl_in_product_dir(productdir)
 
-    # create band stack
-    feedback.pushConsoleInfo("Creating band stack ...")
-    refimg = os.path.join(tempdir, "ref.vrt")
-    create_landsat_stack(
-        productdir, outfile=refimg, imagename="ref", landsatkey=landsatkey
-    )
-    feedback.pushConsoleInfo("Done.")
-
-    # create angles file
-    anglesfile = anglesfile or os.path.join(tempdir, "angles.img")
-    if not os.path.isfile(anglesfile):
-        feedback.pushConsoleInfo("Creating angles file ...")
-        with np.errstate(invalid="ignore"):
-            mainRoutine_angles(
-                Namespace(mtl=mtl, templateimg=refimg, outfile=anglesfile)
-            )
+        # create band stack
+        feedback.pushConsoleInfo("Creating band stack ...")
+        refimg = os.path.join(tempdir, "ref.vrt")
+        create_landsat_stack(
+            productdir, outfile=refimg, imagename="ref", landsatkey=landsatkey
+        )
         feedback.pushConsoleInfo("Done.")
 
-    feedback.pushConsoleInfo("Creating TOA image ...")
-    cmdargs = Namespace(infile=refimg, mtl=mtl, anglesfile=anglesfile, output=outfile)
-    with np.errstate(invalid="ignore"):
-        mainRoutine(cmdargs)
-    feedback.pushConsoleInfo("Done.")
+        # create angles file
+        anglesfile = anglesfile or os.path.join(tempdir, "angles.img")
+        if not os.path.isfile(anglesfile):
+            feedback.pushConsoleInfo("Creating angles file ...")
+            with np.errstate(invalid="ignore"):
+                mainRoutine_angles(
+                    Namespace(mtl=mtl, templateimg=refimg, outfile=anglesfile)
+                )
+            feedback.pushConsoleInfo("Done.")
 
-finally:
-    try:
-        shutil.rmtree(tempdir)
-    except OSError:
-        pass
+        feedback.pushConsoleInfo("Creating TOA image ...")
+        cmdargs = Namespace(infile=refimg, mtl=mtl, anglesfile=anglesfile, output=outfile)
+        with np.errstate(invalid="ignore"):
+            mainRoutine(cmdargs)
+        feedback.pushConsoleInfo("Done.")
 
-dataobjects.load(outfile, os.path.basename(outfile))
+    finally:
+        try:
+            shutil.rmtree(tempdir)
+        except OSError:
+            pass
+
+    dataobjects.load(outfile, os.path.basename(outfile))
